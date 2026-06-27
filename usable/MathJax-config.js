a19f266a2d70fb808374bea5773542d524a070a9
hexiongwu1995_local <hexiongwu1995@outlook.com>
2026-06-28 00:57:26 +0800

    some updates

---
+++ usable/MathJax-config.js
@@ -0,0 +1,12 @@
+MathJax = {
+    loader: {
+        load: ['input/asciimath', 'output/chtml', 'ui/menu']
+    },
+    chtml: { scale: 0.8 },
+};
+// AsciiMath的输入语法请参考这个链接： https://asciimath.org/
+
+// html文件中的使用方式
+// <script src="MathJax-config.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/mathjax@4/startup.js" defer></script>
+
